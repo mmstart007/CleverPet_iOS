5f89257c172390b865dca16d5806e7c321a0c2f1
Dan Wright <dan@fingerfoodstudios.com>
2016-02-16 17:11:16 -0800

    Pet profile setup controller

---
+++ CleverPet/CleverPet/Login/CPPetProfileViewController.m
@@ -0,0 +1,131 @@
+//
+//  CPPetProfileViewController.m
+//  CleverPet
+//
+//  Created by Dan Wright on 2016-02-16.
+//  Copyright © 2016 CleverPet, Inc. All rights reserved.
+//
+
+#import "CPPetProfileViewController.h"
+#import "CPTextField.h"
+
+@interface CPPetProfileViewController ()<UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *headerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subCopyLabel;
+@property (weak, nonatomic) IBOutlet CPTextField *nameField;
+@property (weak, nonatomic) IBOutlet CPTextField *familyField;
+@property (weak, nonatomic) IBOutlet CPTextField *breedField;
+@property (weak, nonatomic) IBOutlet CPTextField *genderField;
+@property (weak, nonatomic) IBOutlet CPTextField *ageField;
+@property (weak, nonatomic) IBOutlet CPTextField *weightField;
+@property (weak, nonatomic) IBOutlet UIButton *continueButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *continueButtonBottomConstraint;
+
+@property (nonatomic, strong) NSArray *textFields;
+
+@end
+
+@implementation CPPetProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // TODO: switch for weight units, gender, auto complete for breeds, potentially switch for altered
+    [self setupStyling];
+    self.textFields = @[self.nameField, self.familyField, self.breedField, self.genderField, self.ageField, self.weightField];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setupStyling
+{
+    self.view.backgroundColor = [UIColor appBackgroundColor];
+    self.headerLabel.text = NSLocalizedString(@"Pet's Profile", @"Title text for pets profile");
+    self.headerLabel.font = [UIFont cpLightFontWithSize:kSignInHeaderFontSize italic:NO];
+    self.headerLabel.textColor = [UIColor appSignUpHeaderTextColor];
+    self.subCopyLabel.text = NSLocalizedString(@"Placeholder sub copy", @"Sub copy text for pets profile");
+    self.subCopyLabel.font = [UIFont cpLightFontWithSize:13.f italic:NO];
+    self.subCopyLabel.textColor = [UIColor colorWithWhite:.5f alpha:1.f];
+    self.continueButton.backgroundColor = [UIColor appLightTealColor];
+    self.continueButton.titleLabel.font = [UIFont cpLightFontWithSize:kButtonTitleFontSize italic:NO];
+    [self.continueButton setTitleColor:[UIColor appTealColor] forState:UIControlStateNormal];
+}
+
+#pragma mark - IBActions
+- (IBAction)continueTapped:(id)sender
+{
+    // TODO: verification
+}
+
+#pragma mark - UITextFieldDelegate methods
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSUInteger index = [self.textFields indexOfObject:textField];
+    if (index < [self.textFields count]) {
+        [self.textFields[index+1] becomeFirstResponder];
+    } else {
+        // TODO: tap continue?
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
+
+#pragma mark - Keyboard
+- (void)keyboardWillShow:(NSNotification *)note
+{
+    NSDictionary *info = [note userInfo];
+    CGRect keyboardRect = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    keyboardRect = [self.view convertRect:keyboardRect fromView:nil];
+    
+    NSTimeInterval duration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationCurve curve = [[info objectForKey:UIKeyboardAnimationCurveUserInfoKey] intValue];
+    
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        [UIView setAnimationCurve:curve];
+        self.continueButtonBottomConstraint.constant = keyboardRect.size.height;
+        [self.view layoutIfNeeded];
+    } completion:nil];
+}
+
+- (void)keyboardWillHide:(NSNotification *)note
+{
+    NSDictionary *info = [note userInfo];
+    NSTimeInterval duration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationCurve curve = [[info objectForKey:UIKeyboardAnimationCurveUserInfoKey] intValue];
+    
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        [UIView setAnimationCurve:curve];
+        self.continueButtonBottomConstraint.constant = 0.f;
+        [self.view layoutIfNeeded];
+    } completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
